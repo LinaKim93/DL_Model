dfa5341365ac4ac607a42711447517c933cbba6c
LinaKim93 <release93@gmail.com>
2022-01-21 01:58:47 +0900

    add: get_train_data

--- train.py
+++ train.py
@@ -1,28 +1,14 @@
-import models.LeNet.lenet as lenet
-import imageio
-import numpy as np
-import cv2
-
-
-def set_dict():
-    dict_list = {}
-    with open('dataset/words.txt', 'r') as f:
-        for line in f.readlines():
-            code = line.split('\t')[0].strip()
-            obj = line.split('\t')[1].strip()
-            dict_list[code] = obj
-
-    return dict_list
+from models.TF.LeNet.Lenet import LeNet
+from utils.CallData import GetTrainData, set_dict
 
 
 def train():
-    img = imageio.imread('dataset/train/n01443537/images/n01443537_1.JPEG')
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
+    pass
 
 
 if __name__ == "__main__":
-    dict_list = set_dict()
-    # model = lenet.LeNet(input_shape=(28, 28, 3))
-    # model.summary()
-    train()
+    # dict_list = set_dict()
+    path = 'dataset'
+    x_train, y_train = GetTrainData(path)
+
+    print(x_train.shape, y_train.shape)
---
+++ utils/CallData.py
@@ -0,0 +1,33 @@
+import glob
+import os
+import numpy as np
+import cv2
+
+
+def set_dict():
+    dict_list = {}
+    with open('dataset/words.txt', 'r') as f:
+        for line in f.readlines():
+            code = line.split('\t')[0].strip()
+            obj = line.split('\t')[1].strip()
+            dict_list[code] = obj
+
+    return dict_list
+
+
+def GetTrainData(path):
+    """
+    This function returns a list of all files in a directory
+    """
+    # Get all files in the directory
+    train_path = os.path.join(path, 'train/*/*/*.JPEG')
+    train_img_path = glob.glob(train_path)
+    train_img = np.array([cv2.imread(img) for img in train_img_path])
+    train_label = np.array(
+        [img.split('/')[-1].split('.')[0].split('_')[0] for img in train_img_path])
+
+    return train_img, train_label
+
+
+def GetValData(path):
+    pass
