b82b8c35bd4f0db157be01425ec8460eefe69fd6
LinaKim93 <release93@gmail.com>
2022-02-23 23:01:37 +0900

    mod: normalize

--- models/TF/utils/tf_imagenet_loader.py
+++ models/TF/utils/tf_imagenet_loader.py
@@ -1,5 +1,5 @@
 import time
-import imageio
+import cv2
 import numpy as np
 import glob
 
@@ -22,7 +22,7 @@ def get_class_to_id_dict():
         n_id, word = line.split('\t')[:2]
         all_classes[n_id] = word
     for key, value in id_dict.items():
-        result[key] = (all_classes[key])
+        result[key] = all_classes[key]
     return result
 
 
@@ -30,19 +30,28 @@ def get_data_classification():
     print("loading data")
     t = time.time()
     train_data = sorted(glob.glob(path+'train/*/images/*.JPEG'))
-
-    X_train = [imageio.imread(img, as_gray=False, pilmode='RGB') for img in train_data]
-    Y_train = [img.split('/')[-3] for img in train_data]
     
-    X_val, Y_val = [], []
-        
+    X_train = np.zeros((100000 , 64, 64, 3), dtype=np.float32)
+    Y_train = []
+    X_val = np.zeros((10000, 64, 64, 3), dtype=np.float32)
+    Y_val = []
+
+    # X_train = [imageio.imread(img, as_gray=False, pilmode='RGB') for img in train_data]
+    # Y_train = [img.split('/')[-3] for img in train_data]
+    for i in range(len(train_data)):
+        X_train[i] = cv2.imread(train_data[i], cv2.IMREAD_COLOR)
+        Y_train.append(train_data[i].split('/')[-3])
+    
+    i = 0
     for line in open(path+'val/val_annotations.txt'):
         img_name, class_id = line.split('\t')[:2]
-        X_val.append(imageio.imread(path+'val/images/'+img_name, as_gray=False, pilmode='RGB'))
+        X_val[i] = cv2.imread(path+'val/images/'+img_name, cv2.IMREAD_COLOR)
         Y_val.append(class_id)
+        i += 1
         
     print('finished loading data, in {} seconds'.format(time.time()-t))
-    return np.array(X_train), np.array(Y_train), np.array(X_val), np.array(Y_val)
+    
+    return X_train, np.array(Y_train), X_val, np.array(Y_val)
 
 
 def get_data_bboxes():
@@ -64,8 +73,9 @@ if __name__ == "__main__":
     print( "test data shape: ",   val_data.shape )
     print( "test_labels shape: ", val_labels.shape )
     
-
     cls_dict = get_class_to_id_dict()
-       
+    train_data /= 255.0
+    val_data /= 255.0
     
+    print(train_data[0])
     print(val_labels[0], cls_dict[val_labels[0]])
--- models/TF/utils/tf_train.py
+++ models/TF/utils/tf_train.py
@@ -1,11 +1,27 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(__file__)))))))  # append your path
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(__file__)))))))))
+
+import numpy as np
+import cv2
+import imageio
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from models.TF.utils.tf_imagenet_loader import *
 
 
 def train():
-    pass
+    batch_size = 64
+    classes = 200
+    epoches = 10
+    
+    img_width, img_height = (64, 64)
+    img_channels = 3
+    
+    train_data, train_labels, val_data, val_labels = get_data_classification(get_id_dict())
+        
+    # normalize
+    
 
 
 if __name__ == "__main__":
-    pass
+    train()
